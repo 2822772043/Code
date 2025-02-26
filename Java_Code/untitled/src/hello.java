d04116e4ba218f1055bd76432c694fb475ae072e
ArlenYZM <a840154b@163.com>
2025-02-26 08:21:45 +0800

    测试hello world git2

--- Java_Code/untitled/src/hello.java
+++ Java_Code/untitled/src/hello.java
@@ -2,6 +2,7 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("hello world ");
         System.out.println("hello git 这是一个测试 ");
+        System.out.println("hello git 这是一个测试 02");
 
     }
 }

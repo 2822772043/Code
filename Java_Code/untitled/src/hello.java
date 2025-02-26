98c60e885b0375748534631da4aa6532e6a7e634
ArlenYZM <a840154b@163.com>
2025-02-26 08:38:06 +0800

    测试代码冲突

--- Java_Code/untitled/src/hello.java
+++ Java_Code/untitled/src/hello.java
@@ -3,6 +3,8 @@ public class hello {
         System.out.println("hello world ");
         System.out.println("hello git 这是一个测试 ");
         System.out.println("hello git 这是一个测试 02");
+        System.out.println("hello git 这是一个冲突测试 02");
+
 
     }
 }
